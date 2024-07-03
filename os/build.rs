0b8ea1b01cfc7e34af856974e9d16c5ded0ccea2
zjp <jiping_zhou@foxmail.com>
2024-07-03 11:08:07 +0800

    clippy: fix on build.rs

--- os/build.rs
+++ os/build.rs
@@ -20,11 +20,10 @@ struct Cases {
 fn insert_app_data() -> Result<()> {
     let mut f = File::create("src/link_app.asm").unwrap();
     let cfg = read_to_string("../user/cases.toml").unwrap();
-    let cases = toml::from_str::<HashMap<String, Cases>>(&cfg)
-        .unwrap()
-        .remove(&format!("usercases"))
+    let cases = toml::from_str::<HashMap<String, Cases>>(&cfg)?
+        .remove("usercases")
         .unwrap_or_default();
-    println!("{:?}", cases);
+    println!("{cases:?}");
     let apps: Vec<_> = cases.cases.unwrap();
 
     writeln!(
@@ -39,7 +38,7 @@ _num_app:
     )?;
 
     for i in 0..apps.len() {
-        writeln!(f, r#"    .quad app_{}_start"#, i)?;
+        writeln!(f, r#"    .quad app_{i}_start"#)?;
     }
     writeln!(f, r#"    .quad app_{}_end"#, apps.len() - 1)?;
 
@@ -50,22 +49,21 @@ _num_app:
 _app_names:"#
     )?;
     for app in apps.iter() {
-        writeln!(f, r#"    .string "{}""#, app)?;
+        writeln!(f, r#"    .string "{app}""#)?;
     }
 
     for (idx, app) in apps.iter().enumerate() {
-        println!("app_{}: {}", idx, app);
+        println!("app_{idx}: {app}");
         writeln!(
             f,
             r#"
     .section .data
-    .global app_{0}_start
-    .global app_{0}_end
+    .global app_{idx}_start
+    .global app_{idx}_end
     .align 3
-app_{0}_start:
-    .incbin "{2}{1}"
-app_{0}_end:"#,
-            idx, app, TARGET_PATH
+app_{idx}_start:
+    .incbin "{TARGET_PATH}{app}"
+app_{idx}_end:"#
         )?;
     }
     Ok(())
